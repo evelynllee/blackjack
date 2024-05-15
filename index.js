c44c722b864811539eb1af7a45f4e1a8af989dfc
Evelyn Lee <evelyn@Evelyns-Air.ht.home>
2024-05-15 19:21:24 -0400

    added a function

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstcardEl = document.getElementById("firstcard-el")
+let secondcardEl = document.getElementById("second-el")
+let hasBlackJack = false
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
+let sum = 0;
+let messageEl = document.getElementById("message-el")
+let startEl= document.getElementById("start-el")
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
-} else if (sum > 21) {
-    console.log("You're out of the game!")
+
+function start(){
+    let firstCard = Math.floor(Math.random() * 12) + 1;
+    firstcardEl.textContent = firstCard;
+    cardsEl.textContent = firstCard;
+    sum += firstCardEl;
+
+    secondcardEl.textContent =  Math.floor(Math.random() * 12) + 1
+
+    while (!hasBlackJack) {
+        if (sum < 21) {
+            messageEl.textContent = "Do you want to draw a new card?"
+            while 
+
+        } else if (sum === 21) {
+            messageEl.textContent = "Wohoo! You've got Blackjack!"
+            hasBlackJack = true;
+
+        } else{
+            messageEl.textContent = "You're out of the game!"
+            hasBlackJack = true;
+        }
+    }
+    startEl.disabled = true
+
 }
