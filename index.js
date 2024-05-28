5258a8f89c65f67afe3c81cbcfaaaa06db947d80
Evelyn Lee <evelyn@Evelyns-Air.ht.home>
2024-05-28 19:58:52 -0400

    updates with bugs

--- index.js
+++ index.js
@@ -1,35 +1,134 @@
-let firstcardEl = document.getElementById("firstcard-el")
-let secondcardEl = document.getElementById("second-el")
-let hasBlackJack = false
-let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
+let messageEl = document.getElementById("message-el");
+let startEl = document.getElementById("start-el");
+let standEl = document.getElementById("stand-el");
+let hitEl = document.getElementById("hit-el");
+let firstCardEl = document.getElementById("firstCard-el");
+let secondCardEl = document.getElementById("secondCard-el");
+let dealersSumEl= document.getElementById("dealersSum-el");
+let dealersFirstEl= document.getElementById("dealersFirst-el");
+let dealersSecondEl= document.getElementById("dealersSecond-el");
+let hitCardEl = document.getElementById("hitCard-el")
+let dealersHitCardEl = document.getElementById("dealersHitCard-el")
+
+hitCardEl.style.display = 'none';
+
+// dealer 
+let dealerCard1 = Math.floor(Math.random() * 10) + 1;
+let dealerCard2 = Math.floor(Math.random() * 10) + 1;
+
+// user's cards
+let first = Math.floor(Math.random() * 10) + 1;
+let second = Math.floor(Math.random() * 10) + 1;
+
+
 let sum = 0;
-let messageEl = document.getElementById("message-el")
-let startEl= document.getElementById("start-el")
+let dealerSum = dealerCard1+dealerCard2;
+let hasBlackJack = false;
+let bust = false;
+
+standEl.style.display = 'none';
+hitEl.style.display = 'none';
 
 
 function start(){
-    let firstCard = Math.floor(Math.random() * 12) + 1;
-    firstcardEl.textContent = firstCard;
-    cardsEl.textContent = firstCard;
-    sum += firstCardEl;
+    sum = first+second;
 
-    secondcardEl.textContent =  Math.floor(Math.random() * 12) + 1
+    firstCardEl.textContent = first
+    secondCardEl.textContent = second
+    dealersFirstEl.textContent = dealerCard1;
 
-    while (!hasBlackJack) {
-        if (sum < 21) {
-            messageEl.textContent = "Do you want to draw a new card?"
-            while 
 
-        } else if (sum === 21) {
-            messageEl.textContent = "Wohoo! You've got Blackjack!"
-            hasBlackJack = true;
+    standEl.style.display = 'block';
+    hitEl.style.display = 'block';
+    sumEl.style.display = 'block';
+    firstCardEl.style.display = 'block';
+    secondCardEl.style.display = 'block';
+    dealersFirstEl.style.display = 'block';
 
-        } else{
-            messageEl.textContent = "You're out of the game!"
-            hasBlackJack = true;
+    startEl.style.display = 'none';
+
+    standEl.disabled = false;
+    hitEl.disabled = false; 
+
+    gameplay();
+
+}
+
+startEl.addEventListener('click', start)
+
+function gameplay() {
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum < 21) {
+        messageEl.textContent = "Stand or hit?"
+
+    } else if (sum === 21) {
+        messageEl.textContent = "Wohoo! You've got Blackjack!"
+        standEl.disabled = true;
+        hitEl.disabled = true;
+
+    } else{
+        messageEl.textContent = "BUST"
+        standEl.disabled = true;
+        hitEl.disabled = true;
+        dealersFirstEl.style.display = 'none';
+
+    }
+}
+
+
+function stand(){
+    standEl.style.display = 'none';
+    hitEl.style.display = 'none';
+
+    dealersSecondEl.textContent = dealerCard2;
+    dealersSumEl.textContent = "Dealer's Sum: " + dealerSum;
+    dealersSecondEl.style.display = 'block';
+    dealersSumEl.style.display = 'block';
+
+
+    if (dealerSum < 17){
+        while (dealerSum < 17){
+            dealerNew();
+        }
+    } else {
+        if (dealerSum > 21){
+        messageEl.textContent = ("The dealer's sum is: " + dealerSum + ". Dealer busted, you win!");
+        } else if (dealerSum > sum){
+            messageEl.textContent = ("The dealer's sum is: " + dealerSum + ". You lose!");
+        } else if (dealerSum == sum){
+            messageEl.textContent = ("The dealer's sum is: " + dealerSum + ". It's a push!");
+        } else {
+            messageEl.textContent = ("The dealer's sum is: " + dealerSum + " You win!");
         }
     }
-    startEl.disabled = true
+    standEl.disabled = true;
+    hitEl.disabled = true;
+
+}
+
+standEl.addEventListener('click', stand);
+
+function hit(){
+
+    hitCard = Math.floor(Math.random() * 10) + 1; 
+    hitCardEl.textContent = "New Card: " + hitCard;
+    hitCardEl.style.display = 'block';
+    sum += hitCard;
+
+    gameplay();
+
+
+}
+
+hitEl.addEventListener('click', hit)
+
+function dealerNew(){
+    let dealerNewCard = Math.floor(Math.random() * 10) + 1; 
+    dealersHitCardEl.textContent = "New Card: " + dealerNewCard;
+    dealersHitCardEl.style.display = 'block';
+    dealerSum += dealerNewCard;
+    dealersSumEl.textContent = "Dealer's Sum: " + dealerSum;
 
 }
